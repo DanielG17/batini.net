5358fd499dd07540f02a015665a99979d0123bd8
DanielG17 <daniel.grech17@gmail.com>
2025-02-09 00:28:26 +0100

    09-02

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the target date
-const targetDate = new Date("February 9, 2025 00:00:00").getTime();
+const targetDate = new Date("March 8, 2025 00:00:00").getTime();
 
 // Update the countdown every second
 const countdownTimer = setInterval(function () {
@@ -7,13 +7,14 @@ const countdownTimer = setInterval(function () {
     const timeLeft = targetDate - now;
 
     // Calculate days, hours, minutes, and seconds
+    const days = Math.floor((timeLeft / (1000 * 60 * 60* 24)));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     // Display the result
     document.getElementById("countdown").innerHTML =
-        `${hours} hrs, ${minutes} min, ${seconds} sec`;
+        `${days} days, ${hours} hrs, ${minutes} min, ${seconds} sec`;
 
     // If the countdown is over, display a message
     if (timeLeft < 0) {
