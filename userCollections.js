9ec134148a9724065ca4423b7cbc3612edc40f6e
DanielG17 <daniel.grech17@gmail.com>
2025-05-31 15:34:25 +0200

    1.1

---
+++ userCollections.js
@@ -0,0 +1,10 @@
+const userCollections = {
+    "John Gatt": {
+        characters: ["Kenneth Micallef", "Luka Haber"],
+        powerups: ["10 Health Boost Common"]
+    },
+    "Kenneth Micallef": {
+        characters: ["Ryan Curmi"],
+        powerups: ["10 Health Boost Common", "10 Damage Boost Epic"]
+    }
+};
